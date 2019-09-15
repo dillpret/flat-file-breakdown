6cc5e108408cf8edf5466103b406262887ebea22
Dillon Pretorius <pretorius.dillon@gmail.com>
2019-09-15 16:48:13 +0200

    venv fix (hopefully)

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Repos\flat-file-breakdown\venv"
+set "VIRTUAL_ENV=F:\Repos\flat-file-breakdown\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
